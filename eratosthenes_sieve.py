76552fc6a0fdaba814c552a6fcb03c256aa69c95
beling <qwak82@gmail.com>
2019-03-10 09:16:19 +0100

    Create eratosthenes_sieve.py

---
+++ eratosthenes_sieve.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Piotr Beling, 2018
+
+def primes():
+    """Generate prime numbers ad infinitum using Sieve of Eratosthenes algorithm."""
+    p = {}
+    n = 2
+    while True:
+        l = p.get(n)
+        if l is None:
+            yield n
+            l = [n]
+        else:
+            del p[n]
+        for v in l:
+            # add v to p[n+v] list
+            k = n+v
+            dest = p.get(k)
+            if dest is None:
+                p[k] = [v]
+            else:
+                dest.append(v)
+        n += 1
+
+if __name__ == "__main__": # demo program:
+    from itertools import takewhile
+    for prime in takewhile(lambda x: x < 100, primes()):
+        print(prime)
